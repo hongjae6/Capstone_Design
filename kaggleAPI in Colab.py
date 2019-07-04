97fe9b424a14372da74e4e35c9e3bdb2723859e9
hongjae <hongjae6@gmail.com>
2019-07-04 12:37:10 +0900

    update 20190704

--- kaggleAPI in Colab.py
+++ kaggleAPI in Colab.py
@@ -6,7 +6,7 @@ from google.colab import files
 files.upload()
 
 # Kaggle API json file
-ls -lha kaggle.json
+!ls -lha kaggle.json
 
 # Make directory and copy to kaggle folder
 
@@ -15,6 +15,6 @@ ls -lha kaggle.json
 !chmod 600 ~/.kaggle/kaggle.json
 
 # Import Kaggle Authentication information
-
-!export KAGGLE_USERNAME='your_user_name'
-!export KAGGLE_KEY='your_authentication_key'
+# json 파일까지 이식 후 되지 않을 경우 아래의 코드 실행
+# !export KAGGLE_USERNAME='your_user_name'
+# !export KAGGLE_KEY='your_authentication_key'
