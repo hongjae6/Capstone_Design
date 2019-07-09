054f3d3d0b7b440a3158c07c4b84f1c4f88c7f87
hongjae <hongjae6@gmail.com>
2019-07-09 16:53:38 +0900

    Update csvToImage.py

--- plant/csvToImage.py
+++ plant/csvToImage.py
@@ -3,21 +3,22 @@ from fastai.vision import *
 # 현재 참조하는 디렉토리의 리스트 불러오기
 import os
 
-path = "./images" # csv 파일이 들어있는 경로 설정
+csv_path = input("Input your csv file path : ")
+unit = input("Input your split unit : ")
+
+path = csv_path # csv 파일이 들어있는 경로 설정
 file_list = os.listdir(path)
 
 print ("file_list: {}".format(file_list))
 
-
 # 파일 이름 추출 확인을 위한 함수
-def fd(name, file_name):
+def file_name_verify(name, file_name):
   print('식물 이름: '+ name)
   print('파일명: ' + file_name)
 
 
 def download_files(name, file_name):
   # 폴더 만들기
-  path = Path('images')
   dest = path/folder
   dest.mkdir(parents=True, exist_ok=True)
   #이미지 다운로드
@@ -25,7 +26,7 @@ def download_files(name, file_name):
 
 # 파일 이름 추출하기
 for i in file_list:
-  fl_ = i.split('.csv')
+  fl_ = i.split(unit)
   del(fl_[1:])
   print(fl_)
   fl_name = ''.join(fl_)
