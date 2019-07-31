76f33891dd5689ff846c535eab777baba7c76cbf
hongjae <hongjae6@gmail.com>
2019-07-31 18:30:28 +0900

    20190731

---
+++ plant/mdpicture/flask_web/app.py
@@ -0,0 +1,28 @@
+from fastai import *
+from fastai.vision import *
+
+from pathlib import Path
+
+from flask import Flask
+app = Flask(__name__)
+
+IMAGE_PATH = 'image'
+MODEL_PATH = 'model'
+MODEL_NAME = 'stage-6'
+
+
+def analysis():
+
+
+
+@app.route('/')
+def main_index():
+    return 'Hello World!'
+
+@app.route('upload')
+def file_upload():
+    return 'Upload Complete!'
+
+
+if __name__ == '__main__':
+    app.run()
